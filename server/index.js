e913d37278e7130e2b2677ea9987aa7b94a6986d
Henil Patel <110276397+Henil00@users.noreply.github.com>
2024-06-26 21:07:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const { app,server } = require('./socket/index')
 // const app = express()
 app.use(cors({
     origin : process.env.FRONTEND_URL,
-    credentials : true
+    credentials : false
 }))
 app.use(express.json())
 app.use(cookiesParser())
