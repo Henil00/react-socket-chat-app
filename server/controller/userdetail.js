cbaa12223667a083474818eefbbd21a7cb6060ec
Henil Patel <110276397+Henil00@users.noreply.github.com>
2024-06-27 11:05:09 +0530

    Update userdetail.js

--- server/controller/userdetail.js
+++ server/controller/userdetail.js
@@ -3,12 +3,13 @@ const getuserdetailfromtoken = require("../helpers/getuserdetailfromtoken")
 async function userdetail(req,res){
     try {
         const token = req.cookies.token || ""
-
+        console.log(token);
         const user = await getuserdetailfromtoken(token)
 
         return res.status(200).json({
             message : "User Detail",
-            data : user
+            data : user,
+            token: token
         })
 
     } catch (error) {
